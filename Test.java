92aaac67a1775ba4ae9c7b68c2ce18352824b529
aalam <aalam.velociters@gmail.com>
2021-03-23 15:19:37 +0530

    Test.java commit after modifieng

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ package com.velociters;
 public class Test {
 public static void main(String a [])
 {
-	for(int i=1 ; i<=19 ; i++)
+	for(int i=1 ; i<=20 ; i++)
 	{
 		System.out.printf("%o\n",i);
 	}
